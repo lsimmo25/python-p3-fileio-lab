494aa77df03b64f545e730ea5a71a8600df61c62
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-25 22:08:20 -0400

    lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name: str, file_content):
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "r") as f:
+        return f.read()
